8d683a0172e6b25e18b6ba6f1642846f61fa24e1
李志雄 <982594533@qq.com>
2020-09-25 16:39:17 +0800

    测试

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=qingzhan',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=192.168.10.63;dbname=hzwebsitedb',
+    'username' => 'jiahuirong',
+    'password' => 'jhV94j3LtHKQsPwR',
     'charset' => 'utf8',
     'tablePrefix' => 'api_',
     // Schema cache options (for production environment)
